58132f87b67dc640617c3e03ac694356ec9c84c4
Vicfred <vicfredboy@gmail.com>
2019-06-25 19:27:56 -0500

    multiple case problem, replicateM, sequence_ example

---
+++ waytoolongwordsV2.hs
@@ -0,0 +1,17 @@
+-- Vicfred
+-- https://codeforces.com/contest/71/problem/A
+
+import Control.Monad
+
+solve :: String -> String
+solve s =
+    let len = length s
+    in if len <= 10
+        then s
+        else ([head s] ++ show (len - 2) ++ [last s])
+
+main = do
+    n <- getLine
+    inputs <- replicateM (read n) getLine
+    let answers = map solve inputs
+    sequence_ (map putStrLn answers)
