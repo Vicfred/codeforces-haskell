c3f6b525f4f6e4387d947fdb112bf748264b8720
Vicfred <vicfredboy@gmail.com>
2019-06-25 19:52:12 -0500

    words, getline, foldl, lambda example

---
+++ nextround.hs
@@ -0,0 +1,7 @@
+main = do
+    input <- getLine
+    let [n, k] = map read (words input)
+    input2 <- getLine
+    let l = map read (words input2)
+    let cutoff = max 1 (l !! (k - 1))
+    putStrLn $ show (foldl (\acc x -> if x >= cutoff then (acc +  1) else acc) 0 l)
