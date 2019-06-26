837d7e20b0b8491f2a97818ecd458089802848ef
Vicfred <vicfredboy@gmail.com>
2019-06-25 22:46:27 -0500

    interact, toupper, string order comparison, slicing

---
+++ petyaandstrings.hs
@@ -0,0 +1,7 @@
+-- Vicfred
+-- https://codeforces.com/problemset/problem/112/A
+
+import Data.Char
+
+main = interact $ show . f . map (map toUpper) . lines
+f (a:b:c) = if a == b then 0 else (if a < b then -1 else 1)
