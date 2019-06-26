549440e1522e2329f85826eb37d68633ce9d4078
Vicfred <vicfredboy@gmail.com>
2019-06-25 21:01:02 -0500

    fmap, IO monad, composition

---
+++ team.hs
@@ -0,0 +1,15 @@
+-- Vicfred
+-- https://codeforces.com/problemset/problem/231/A
+
+import Control.Monad
+
+main = do
+    n <- readLn
+    a <- replicateM n getList
+    putStrLn . show $ solve a
+
+solve :: [[Int]] -> Int
+solve = length . filter ((>= 2) . sum)
+
+getList :: Read a => IO [a]
+getList = fmap (map read . words) getLine
