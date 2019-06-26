9eadad92a6c8b932c04da76cad6611f3542641de
Vicfred <vicfredboy@gmail.com>
2019-06-25 20:07:19 -0500

    filter, lambda, words, read

---
+++ nextround_.hs
@@ -0,0 +1,10 @@
+-- Vicfred
+-- https://codeforces.com/problemset/problem/158/A
+
+main = do
+    input <- getLine
+    let [n, k] = map read (words input)
+    input2 <- getLine
+    let l = map read (words input2)
+    let cutoff = max 1 (l !! (k - 1))
+    putStrLn $ show (length $ filter (\x -> x >= cutoff) l)
