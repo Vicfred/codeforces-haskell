6a6d321c0fa575a54b6c850709711124b7d00b3f
Vicfred <vicfredboy@gmail.com>
2019-07-05 16:32:05 -0500

    simple getIntList, where, composition, >>=

--- evenodds.hs
+++ evenodds.hs
@@ -1,7 +1,9 @@
 -- Vicfred
 -- https://codeforces.com/problemset/problem/318/A
 
-solve :: [Int] -> Int
+import Data.Int
+
+solve :: [Int64] -> Int64
 solve [n,k] = if k <= m then 2*k-1 else 2*(k-m)
               where m = (n+1) `div` 2
 
