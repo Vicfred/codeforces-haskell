d54ea1d5be41cfa4ac87fbfab257515e319afd80
Vicfred <vicfredboy@gmail.com>
2019-07-09 21:42:57 -0500

    replicateM, simple getintlist, list comprehension, delete

---
+++ games.hs
@@ -0,0 +1,8 @@
+import Control.Monad
+import Data.List
+
+main :: IO ()
+main = do
+    n <- fmap read getLine
+    a <- replicateM n $ fmap (map read . words) getLine :: IO [[Int]]
+    print $ length [() | i <- a, j <- delete i a, head i == last j]
