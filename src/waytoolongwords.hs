cd55271826dedc1f4a8c163a06983df55b4bc869
Vicfred <vicfredboy@gmail.com>
2019-10-08 23:31:56 -0500

    moved things around

--- src/waytoolongwords.hs
+++ src/waytoolongwords.hs
@@ -1,13 +1,14 @@
--- Vicfred
--- Codeforces
--- Way Too Long Words https://codeforces.com/problemset/problem/71/A
-
-main =  do
-    w <- getLine
-    words <- getContents
-    putStrLn . unlines . map transform . lines $ words
+-- https://codeforces.com/problemset/problem/71/A
 
 transform :: String -> String
 transform s
     | (length s <= 10) = s
     | otherwise = ([head s] ++ show (length s - 2) ++ [last s])
+
+main :: IO ()
+main =  do
+    w <- getLine
+    words <- getContents
+    putStrLn . unlines . map transform . lines $ words
+
+
