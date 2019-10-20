1b0e5b75b4f4133bdb43a32bb6610b865574cfd1
Vicfred <vicfredboy@gmail.com>
2019-10-20 00:58:01 -0500

    function signature, formatting

--- src/petyaandstrings.hs
+++ src/petyaandstrings.hs
@@ -1,7 +1,9 @@
--- Vicfred
 -- https://codeforces.com/problemset/problem/112/A
 
 import Data.Char
 
+main :: IO ()
 main = interact $ show . f . map (map toUpper) . lines
+
 f (a:b:c) = if a == b then 0 else (if a < b then -1 else 1)
+
